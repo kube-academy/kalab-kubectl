50738d4fb8d25b092e32995c7e77b7be18d44bfb
Eitan Suez <esuez@pivotal.io>
2021-03-31 10:54:39 -0500

    one-off test

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -3,3 +3,9 @@
 cat >> ~/.bashrc << EOF
 set -o vi
 EOF
+
+# testing installing kubens/kubectx ..
+git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+mkdir ~/bin
+ln -s ~/.kubectl/kubens ~/bin/
+ln -s ~/.kubectl/kubectx ~/bin/
